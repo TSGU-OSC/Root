b9959ec3487b68e0ca530696c265d3b412eb085e
Jack_Lau <11923890+Jack__Lau@user.noreply.gitee.com>
2023-04-21 14:54:30 +0000

    add C++/刘奇翰的代码/gobangUI/gobang.h.
    Signed-off-by: Jack_Lau <11923890+Jack__Lau@user.noreply.gitee.com>

---
+++ C++/刘奇翰的代码/gobangUI/gobang.h
@@ -0,0 +1,21 @@
+#define u (int)32
+#define Black 1
+#define White 2
+
+void BGM();
+
+void BoardPrint();
+
+void BlackSet();
+
+void WhiteSet();
+
+int Judgement();
+
+void EXE();
+
+void Reset();
+
+int Gobang_Exe(int color);
+
+int gobang[15][15] = { 0 };
